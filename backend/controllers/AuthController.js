e6243ca86157d0ce44019962f59b84efbf7fca09
Vignesh-737 <5737vignesh@gmail.com>
2026-05-04 17:50:22 +0530

    clean start

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,63 @@
+import User from "../models/UserSchema.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+//register
+export const register=async(req,res)=>{
+    try {
+        const {name,email,password}=req.body;
+
+        const exists=await User.findOne({email})
+
+        if(exists){
+            return res.status(400).json({Error:"User Already exists"});
+        }
+
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        const user=await User.create({
+            name,
+            email,
+            password:hashedPassword,
+        });
+
+        res.status(201).json({ message: "User registered" });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+//login
+export const login=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const user=await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({Error:"User not found"});
+        }
+
+        const isMatch=await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({Error:"Credential invalid"});
+        }
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "1hr" }
+
+            
+    );
+     res.json({
+      token,
+      user: {
+        id: user._id,
+        role: user.role
+      }
+    });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
+import testRoutes from "./routes/testRoutes.js";
+
+dotenv.config()
+const app=express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("Backend Running")
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/test", testRoutes);
+
+const connectDB=async()=>{
+    try {
+
+       await mongoose.connect(process.env.MONGO_URI);
+        console.log("MOngoDb Connected")
+    
+    app.listen(5000,()=>{
+    console.log("Server running on port 5000")
+    })  
+    } catch (error) {
+        console.log("Failed!!!  ",error.message)
+        process.exit(1);
+    }
+}
+
+connectDB();
