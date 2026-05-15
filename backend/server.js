f075b187b5d44c988b76e3158de15db6275d2306
Vignesh-737 <5737vignesh@gmail.com>
2026-05-15 22:00:00 +0530

    remove unused test routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose";
 import authRoutes from "./routes/AuthRoutes.js";
-import testRoutes from "./routes/testRoutes.js";
 import medicineRoutes from "./routes/MedicineRoutes.js";
 import stockRoutes from "./routes/stockRoutes.js";
 import adminRoutes from "./routes/AdminRoutes.js";
@@ -17,7 +16,6 @@ app.use(cors())
 
 
 app.use("/api/auth", authRoutes);
-app.use("/api/test", testRoutes);
 app.use("/api/medicines", medicineRoutes);
 app.use("/api/stock", stockRoutes);
 app.use("/api/admin", adminRoutes);
